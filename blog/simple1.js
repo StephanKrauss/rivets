4465b0eb44c2dfa9bdaa1e86bea1578446f256e6
Stephan Krauß <stephan.krauss@haufe.com>
2018-10-03 09:39:42 +0200

    Koppelung eines Javascript Model mit dem Template

--- blog/simple1.js
+++ blog/simple1.js
@@ -1,14 +1,19 @@
-var auction = {
-    product: {
-        name: 'Produkt Name',
-        currentBid: 120,
-        timeLeft: 20
+var mySubTemplateModel = function()
+{
+    // private
+
+    // public
+    return{
+        name: 'Krauss',
+        firstname: 'Stephan',
+        phone: '+0049 176 / 37120235',
+        birthday: '10.07.1958'
     }
-};
+}();
+
 
 
 $( document ).ready(function() {
     console.log('simple 1');
-
-    rivets.bind($('#auction'), auction);
+    rivets.bind($('#mySubtemplate'), mySubTemplateModel);
 });
